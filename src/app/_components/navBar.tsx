e9cd22c5a869d0654cddec7b33393bc66e299548
Ekene <ekeneogwurumba@gmail.com>
2025-01-23 20:15:24 +0100

    Created the navbar and landing page

--- src/app/_components/navBar.tsx
+++ src/app/_components/navBar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const linkStyle ={textDecoration: "none", color: "black", fontSize: "1rem"};
   const hoverStyle = {color: "#2093D6"};
   return (
-    <nav style={{ display: "flex", justifyContent: "end", gap: "8grem", padding: "1.5rem", background: "#F5F3F3" }}>
+    <nav style={{ display: "flex", justifyContent: "end", gap: "8rem", padding: "1.5rem", background: "#F5F3F3" }}>
       <a href="/home" style={linkStyle}
         onMouseEnter={(e) => (e.target.style.color = hoverStyle.color)}
         onMouseLeave={(e) => (e.target.style.color = linkStyle.color)}>
