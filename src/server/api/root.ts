55ffd2be8f25f5cd9d98508b736fd853372882d6
Williams <williamsnkama@gmail.com>
2025-01-29 17:31:42 +0100

    fixed routes referencing post.ts

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
-import { postRouter } from "~/server/api/routers/post";
 import { userRouter } from "~/server/api/routers/user"
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+// import { postRouter } from "~/server/api/routers/post";
 
 /**
  * This is the primary router for your server.
@@ -8,7 +8,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  // post: postRouter,
   user: userRouter
 });
 
