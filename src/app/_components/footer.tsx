55825eac6aa1daf629fbd17e8d1d62323f35af66
Williams <williamsnkama@gmail.com>
2025-01-29 17:21:08 +0100

    fixed footer style bug

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
     
     return (
       <footer style={{ margin: '0', padding: '0.5rem', backgroundColor: '#f5f3f3' }}>
-        <p style={paragraph}>&copy; 2025 Group 16. All rights reserved.</p>
-        <p style={paragraph}>Our mission: To support and connect volunteers worldwide.</p>
+        <p className="text-[#2093D6] bg-[#F5F3F3] p-1 text-base text-center">&copy; 2025 Group 16. All rights reserved.</p>
+        <p className="text-[#2093D6] bg-[#F5F3F3] p-1 text-base text-center">Our mission: To support and connect volunteers worldwide.</p>
       </footer>
     );
   };
